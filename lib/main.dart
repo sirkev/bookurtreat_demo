e7029f8631728b90f7e3adbbd4e75edb1df303e0
kev <kevinoduor8877@gmail.com>
2024-01-08 15:06:43 +0300

    Revert "fix remove firebase core"
    This reverts commit 18c73cf038dfda1206121740457c03fabc7b9afb.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-Future main() async {
+Future main() async{
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
